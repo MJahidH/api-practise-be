d9e4db73612b48a386869190d0ea03db1570e03b
MJahidH <m.jahid.h@hotmail.com>
2024-06-23 16:29:17 +0100

    2

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require("../app.js");
 
-// app.listen(8080, () => {
-//   // console.log("server is running right now on 8080");
-// });
+app.listen(8080, () => {
+  // console.log("server is running right now on 8080");
+});
 
 module.exports = app;
